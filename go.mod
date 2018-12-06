5b01a04a91c58501bc971a01f5fb6db07b6cd84a
develop <ikhlas.firlana@gmail.com>
2018-12-06 17:31:36 +0700

    no message

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ikhlas-firlana/hello-go
+
+require github.com/ikhlas-firlana/hello-go-mod v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	hello "github.com/ikhlas-firlana/hello-go-mod"
+)
+
+func main() {
+	fmt.Println(hello.Hello())
+}
